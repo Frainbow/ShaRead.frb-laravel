fe80d257d617a991fcd55f83f2285133d3b4ee74
nagi <nagi31904@gmail.com>
2016-07-02 22:39:06 +0800

    model

---
+++ database/migrations/2016_07_02_142957_create_book_lists_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateBookListsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('book_lists', function (Blueprint $table) {
+            $table->increments('id');
+            $table->timestamps();
+            $table->integer('store_id');
+            $table->integer('book_id');
+            $table->integer('book_case_id');
+            $table->integer('category_id');
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('book_lists');
+    }
+}
